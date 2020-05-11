424b26d8054c08f51f44770bfae9b832abd2a9a0
sheridjohn <sheridjohn@gmail.com>
2020-05-11 16:44:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+//localStorage.setItem('darkMode', 'Disabled');
+var darkMode = localStorage.getItem('darkMode');
+//get darkMode value from local storage
+function checkAndEnableDarkMode(){
+  console.log("Inside onload function " +darkMode);
+  var bodyElement = document.body;
+  if(darkMode==="Enabled")
+  {
+    bodyElement.classList.toggle("darkMode");
+    document.getElementById('modeToggle').innerHTML = "Light Mode";
+  }
+  bodyElement.style.visibility = "visible";
+}
+/*when the page loads first, it goes with the standard css regardless, so we check
+if dark mode is enabled in local storage, and if so we toggle it on.*/
+
+function toggleMode() {
+   var bodyElement = document.body;
+
+   if(darkMode === "Enabled")
+   {
+     bodyElement.classList.toggle("darkMode");
+     localStorage.setItem('darkMode', 'Disabled');
+     darkMode = localStorage.getItem('darkMode');
+     document.getElementById('modeToggle').innerHTML = "Dark Mode";
+     console.log(darkMode);
+   }else{
+     bodyElement.classList.toggle("darkMode");
+     localStorage.setItem('darkMode', 'Enabled');
+     darkMode = localStorage.getItem('darkMode');
+     document.getElementById('modeToggle').innerHTML = "Light Mode";
+     console.log(darkMode);
+   }
+   /*This function toggles the mode on a button click. If the mode
+   is enabled when clicked, then the click disabls it, and vice versa.
+   Every time we change the mode we need to save information indicating darkModes
+   state in local storage, then update the darkMode variable with that value for
+   us to check. */
+ }
